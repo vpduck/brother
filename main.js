21cad52250f0ff80fe4d56af0622e756f45ef53b
vpduck <84106586+vpduck@users.noreply.github.com>
2021-12-01 19:47:06 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+
+
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gOjy9FhEN/model.json',modelLoaded);
